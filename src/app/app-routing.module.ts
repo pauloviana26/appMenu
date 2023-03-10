6de5e3c4fa4eb9ec7b451adaf29d0f0079b3f46e
Paulo Cesar Faria Viana <paulo.viana@aitec.pt>
2023-03-10 00:28:00 +0000

    pages generated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,42 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'alerts',
+    loadChildren: () => import('./alerts/alerts.module').then( m => m.AlertsPageModule)
+  },
+  {
+    path: 'inputs',
+    loadChildren: () => import('./inputs/inputs.module').then( m => m.InputsPageModule)
+  },
+  {
+    path: 'lists',
+    loadChildren: () => import('./lists/lists.module').then( m => m.ListsPageModule)
+  },
+  {
+    path: 'checkbox',
+    loadChildren: () => import('./checkbox/checkbox.module').then( m => m.CheckboxPageModule)
+  },
+  {
+    path: 'cards',
+    loadChildren: () => import('./cards/cards.module').then( m => m.CardsPageModule)
+  },
+  {
+    path: 'range',
+    loadChildren: () => import('./range/range.module').then( m => m.RangePageModule)
+  },
+  {
+    path: 'toast',
+    loadChildren: () => import('./toast/toast.module').then( m => m.ToastPageModule)
+  },
+  {
+    path: 'badges',
+    loadChildren: () => import('./badges/badges.module').then( m => m.BadgesPageModule)
+  },
+  {
+    path: 'toggles',
+    loadChildren: () => import('./toggles/toggles.module').then( m => m.TogglesPageModule)
+  },
 ];
 
 @NgModule({
